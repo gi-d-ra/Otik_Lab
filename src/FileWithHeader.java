2786e2afaf4bffef3da39c9886af38c319d081ed
Nicholas Romanchuk <roman4uk.nick@gmail.com>
2020-11-02 03:12:07 +0300

    Initial commit & added header parser

---
+++ src/FileWithHeader.java
@@ -0,0 +1,111 @@
+import java.io.*;
+import java.nio.ByteBuffer;
+
+/**
+ * header:
+ * 0-3  -4b-sign
+ * 4-5  -2b-version
+ * 6    -1b-compression types
+ * 7    -1b-lab number
+ * 8    -1b-files count
+ * 9-15 -8b-reserved
+ * 16-19 -4b-source file length
+ * 20-23 -4b-data length
+ * 24-27 -4b-subheader length
+ * 28-31 -4b-reserved
+ */
+
+public class FileWithHeader {
+
+    private String sign;
+    private int version;
+    private int compressionType;
+    private int labNumber;
+    private int filesCount;
+    private int sourceFileLength;
+    private int dataLength;
+    private int subheaderLength;
+
+    InputStream in;
+    File file;
+
+    public FileWithHeader(String file) {
+        try {
+            this.file = new File(file);
+            this.in = new FileInputStream(this.file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // считываем кусками заголовок и заполняем соответсвующие поля
+    public void getHeader() {
+        try {
+            byte[] buff = new byte[4];
+            in.read(buff);
+            sign = new String(buff);
+
+            version = readBytes(in, 2);
+            compressionType = in.read();
+            labNumber = in.read();
+            filesCount = in.read();
+            sourceFileLength = readBytes(in, 2);
+            dataLength = readBytes(in, 4);
+            dataLength = readBytes(in, 4);
+            subheaderLength = readBytes(in, 4);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getSign() {
+        return sign;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public int getCompressionType() {
+        return compressionType;
+    }
+
+    public int getLabNumber() {
+        return labNumber;
+    }
+
+    public int getFilesCount() {
+        return filesCount;
+    }
+
+    public int getSourceFileLength() {
+        return sourceFileLength;
+    }
+
+    public int getDataLength() {
+        return dataLength;
+    }
+
+    public int getSubheaderLength() {
+        return subheaderLength;
+    }
+
+    // чтение и преобразование байтов для полей
+    public static int readBytes(InputStream in, int amount) throws IOException {
+        byte[] buff = new byte[amount];
+        in.read(buff);
+        return parseBytesToInt(buff);
+    }
+
+    // преобразует byte[] в int
+    public static int parseBytesToInt(byte[] arr) {
+        int i = arr.length - 1;
+        int result = 0;
+        for (byte b : arr) {
+            result += b * Math.pow(10, i);
+            i--;
+        }
+        return result;
+    }
+
+}
